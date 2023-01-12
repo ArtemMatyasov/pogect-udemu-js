4aa7af097079ac4491f50080d33383cf2e332dfb
Artem Matyasov <matyasov.86@gmail.com>
2023-01-12 12:38:51 +0300

    Prilogenie1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 "use strict";
-//const result = confirm("Are you hhere?");
-//console.log(result);
-//const answer = +prompt("Вам Усть 18?", "18");
-//console.log(answer + 5);
-const answers = [];
 
-answers[0] = prompt("wats your name?", "");
-answers[1] = prompt("wats your soName?", "");
-answers[2] = prompt("how ?", "");
+const numberOfFilms = prompt("Сколько Фильмов вы посмотрели?", "");
 
-document.write(answers);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
