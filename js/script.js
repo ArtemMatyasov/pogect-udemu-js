72e38b6848fbb752b3fa0616e041d7a3034b1d44
Artem Matyasov <matyasov.86@gmail.com>
2023-01-12 10:06:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//const result = confirm("Are you hhere?");
+//console.log(result);
+//const answer = +prompt("Вам Усть 18?", "18");
+//console.log(answer + 5);
+const answers = [];
+
+answers[0] = prompt("wats your name?", "");
+answers[1] = prompt("wats your soName?", "");
+answers[2] = prompt("how ?", "");
+
+document.write(answers);
