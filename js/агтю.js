596ccd8669ba759d1ef46e86517985478eebf50b
Artem Matyasov <matyasov.86@gmail.com>
2023-01-17 16:09:03 +0300

    Praktika hcrr2

---
+++ js/агтю.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function showFirstMassage(s) {
+  console.log(s);
+}
+showFirstMassage("hello");
+
+function cacl(a, b) {
+  return a + b;
+}
+console.log(cacl(4, 3));
+console.log(cacl(6, 9));
+console.log(cacl(5, 3));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+const anatherNum = ret();
+console.log(anatherNum);
+const loger = function () {
+  console.log("helooy");
+};
+const calc = (a, b) => {
+  return a + b;
+};
